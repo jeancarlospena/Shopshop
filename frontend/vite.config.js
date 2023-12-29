93fb9d69b9bac7ac20cdd4cdf2f89361157fc6b8
Carlos <jcpacc6@outlook.com>
2023-12-29 18:55:49 -0500

    enviroment fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+const enviroment = process.env.NODE_ENVIROMENT === 'development' ? "http://localhost:5000" : 'https://shopshop.onrender.com'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:5000",
-      '/uploads': 'http://localhost:5000'
+      "/api": enviroment,
+      '/uploads': enviroment
     },
   },
   plugins: [react()],
